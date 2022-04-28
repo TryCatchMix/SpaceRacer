ac850d0d9008f7a3fa48f9294ad9206dc4545422
Taky92 <104558876+Taky92@users.noreply.github.com>
2022-04-28 11:05:50 +0200

    SpaceRacer Base

---
+++ src/es/iespolitecnicomalaga/spaceracer/EscenarioJuego.java
@@ -0,0 +1,10 @@
+package es.iespolitecnicomalaga.spaceracer;
+
+/*public class EscenarioJuego extends Escenario{
+
+    //Definimos los objetos que van a aparecer en esta escena
+    NavesAliadas miNave;
+    Obstaculo asteroide;
+    PanelNumeros puntuacion;
+
+}*/
